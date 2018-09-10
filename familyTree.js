2c12d751c944bc346b98caec1bbf1fe70032720b
Tyler Trout <tiller1010@gmail.com>
2018-09-10 14:26:20 -0400

    step 2

--- familyTree.js
+++ familyTree.js
@@ -1,7 +1,10 @@
+let familyCount=[];
+
 function person(name,gender,relation){
 this.name=name,
 this.gender=gender,
 this.relation=relation
+familyCount.push(this);
 }
 person.prototype.sayName=function(){
 console.log(this.name);
@@ -10,5 +13,19 @@ console.log(this.name);
 function familyMember(){}
 familyMember.prototype=Object.create(person.prototype);
 
-let auntie = new person("Suzy","female","r");
+let auntie = new person("Suzy","female","Mother");
 auntie.constructor=familyMember;
+
+let findMothers=function(){
+let mothers=[];
+let regEx=/Mother/;
+for(i=0;i<familyCount.length;i++){
+if(regEx.test(familyCount[i].relation)){
+mothers.push(familyCount[i]);
+}
+}
+console.log(mothers);
+}
+
+console.log(familyCount);
+findMothers();
