6f2a380d1a98f70835f680062cbbb6862636a8ad
Tyler Trout <tiller1010@gmail.com>
2018-09-10 20:15:43 -0400

    also the new js

--- js/familyTree.js
+++ js/familyTree.js
@@ -1,14 +1,14 @@
 let familyCount=[];
 
 function person(name,gender,relation){
-this.name=name,
-this.gender=gender,
-this.relation=relation
-familyCount.push(this);
+    this.name=name;
+    this.gender=gender;
+    this.relation=relation;
+    familyCount.push(this);
 }
 person.prototype.sayName=function(){
-console.log(this.name);
-}
+    //console.log(this.name);
+};
 
 function familyMember(){}
 familyMember.prototype=Object.create(person.prototype);
@@ -16,16 +16,25 @@ familyMember.prototype=Object.create(person.prototype);
 let auntie = new person("Suzy","female","Mother");
 auntie.constructor=familyMember;
 
-let findMothers=function(){
-let mothers=[];
-let regEx=/Mother/;
-for(i=0;i<familyCount.length;i++){
-if(regEx.test(familyCount[i].relation)){
-mothers.push(familyCount[i]);
-}
-}
-console.log(mothers);
-}
+let meghan = new person("Meghan","female","Mother");
+meghan.constructor=familyMember;
 
-console.log(familyCount);
-findMothers();
+let uncle = new person("Dave","male","Father");
+uncle.constructor=familyMember;
+
+let findMothers=function(){
+    let mothers=[];
+    let regEx=/Mother/;
+    for(i=0;i<familyCount.length;i++){
+        if(regEx.test(familyCount[i].relation)){
+            mothers.push(familyCount[i].name);
+        }
+    }
+    	document.getElementById("momCount").innerHTML=mothers;
+
+};
+
+window.onload=function(){
+	let momButton = document.getElementById("momBtn");
+	momButton.addEventListener("click",findMothers,false);
+}
