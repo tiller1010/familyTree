ae94f89638c158600fb64e0703c8dbe04698873e
Tyler Trout <tiller1010@gmail.com>
2018-09-17 18:52:59 -0400

    images can now be added to new person boxes

--- js/familyTree.js
+++ js/familyTree.js
@@ -56,6 +56,7 @@ window.onload=function(){
         document.getElementById("nameBox").value='';
         document.getElementById("genderBox").value='';
         document.getElementById("relationBox").value='';
-        $("#generation3").append("<div class='personBox'</div>");
+        $("#generation3").append("<div class='personBox' style='background-image:url("+document.getElementById("imageBox").value+");'</div>");
+        document.getElementById("imageBox").value='';
     },false);
 }
