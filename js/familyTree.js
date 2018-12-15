dc4b78d8e5e53ba320f8f3c74d68f78ac90e31d8
Tyler Trout <tiller1010@gmail.com>
2018-12-15 17:18:22 -0500

    working creation for first person

--- js/familyTree.js
+++ js/familyTree.js
@@ -1,31 +1,21 @@
-let familyCount=[];
-let familyID=2;
+var familyCount=[];
 
-function person(name,gender,relation,spouse){
+function person(name,gender,parenthood,spouse){
     this.name=name;
     this.gender=gender;
-    this.relation=relation;
+    this.parenthood=parenthood;
     this.spouse=spouse;
     familyCount.push(this);
 }
 
-function familyMember(){}
-familyMember.prototype=Object.create(person.prototype);
-
-let suzy = new person("Suzy","female","Mother");
-suzy.constructor=familyMember;
-
-let meghan = new person("Meghan","female","Mother");
-meghan.constructor=familyMember;
-
-let dave = new person("Dave","male","Father");
-dave.constructor=familyMember;
+// let suzy = new person("Suzy","female","Mother");
+// suzy.constructor=familyMember;
 
 let findFathers=function(){
     let fathers=[];
     let regEx=/Father/i;
     for(i=0;i<familyCount.length;i++){
-        if(regEx.test(familyCount[i].relation)){
+        if(regEx.test(familyCount[i].parenthood)){
             fathers.push(familyCount[i].name);
         }
     }
@@ -36,7 +26,7 @@ let findMothers=function(){
     let mothers=[];
     let regEx=/Mother/i;
     for(i=0;i<familyCount.length;i++){
-        if(regEx.test(familyCount[i].relation)){
+        if(regEx.test(familyCount[i].parenthood)){
             mothers.push(familyCount[i].name);
         }
     }
@@ -77,34 +67,37 @@ window.onload=function(){
 
     let createButton = document.getElementById("create");
     createButton.addEventListener("click",function(){
-        familyID+=1;
-        new person(document.getElementById("nameBox").value,document.getElementById("genderBox").value,document.getElementById("relationBox").value);
-        document.getElementById("nameBox").value='';
-        document.getElementById("genderBox").value='';
-        document.getElementById("relationBox").value='';
-        let newPerson=$("<div class='personBox' style='background-image:url("+document.getElementById("imageBox").value+");'><div class='description'>This is "+familyCount[familyID].name+"</div></div>").on('click',function(){
+        let name1=document.getElementById("nameBox1").value;
+        let gender1=document.getElementById("genderBox1").value;
+        let parenthood1=document.getElementById("parentBox1").value;
+        let image1=document.getElementById("imageBox1").value;
+        let newPerson=new person(name1,gender1,parenthood1);
+        let newFrame=$("<div class='personBox' style='background-image:url("+image1+");'><div class='description'>This is "+newPerson.name+"</div></div>").on('click',function(){
             $(this).children().slideToggle(300);
         });
-        $("#generation3").append(newPerson);
-        document.getElementById("imageBox").value='';
+        $("#generation1").append(newFrame);
+        document.getElementById("nameBox1").value='';
+        document.getElementById("genderBox1").value='';
+        document.getElementById("relationBox1").value='';
+        document.getElementById("imageBox1").value='';
     },false);
 
-    for(i=0;i<familyCount.length;i++){
-	    let canvas = document.getElementById('lines'+i);
-		let ctx = canvas.getContext('2d');
-		ctx.beginPath();
-		ctx.moveTo(90,0);
-		ctx.lineTo(90,70);
-		if(familyCount[i].relation==='Mother'){
-			ctx.lineTo(250,70);
-			ctx.stroke();
-			console.log(familyCount[i]);
-		}
-		if(familyCount[i].relation==='Father'){
-			ctx.lineTo(0,70);
-			ctx.stroke();
-			console.log(familyCount[i]);
-		}
-	}
+ //    for(i=0;i<familyCount.length;i++){
+	//     let canvas = document.getElementById('lines'+i);
+	// 	let ctx = canvas.getContext('2d');
+	// 	ctx.beginPath();
+	// 	ctx.moveTo(90,0);
+	// 	ctx.lineTo(90,70);
+	// 	if(familyCount[i].relation==='Mother'){
+	// 		ctx.lineTo(250,70);
+	// 		ctx.stroke();
+	// 		console.log(familyCount[i]);
+	// 	}
+	// 	if(familyCount[i].relation==='Father'){
+	// 		ctx.lineTo(0,70);
+	// 		ctx.stroke();
+	// 		console.log(familyCount[i]);
+	// 	}
+	// }
 }
 
